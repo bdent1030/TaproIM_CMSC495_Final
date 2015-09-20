0a5dab422c1a974b16e74767cc5be6bd03890067
spencerward7 <spencer.ward7@gmail.com>
2015-09-20 11:05:12 -0500

    init

---
+++ src/taproim_cmsc495/TaproIM_CMSC495.java
@@ -0,0 +1,46 @@
+//Gunar Gorder, Brandon Dent, Frank Montoya, Kent Kenyon, Spencer Ward
+//TAMPROW IM FINAL PROJECT
+//CMSC 495 6380
+//9-5-15
+//TaproIM_CMSC495.java
+package taproim_cmsc495;
+
+
+public class TaproIM_CMSC495 {
+
+    
+    public static void main(String[] args) {
+     /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+               
+                new Login_GUI().setVisible(true);
+            }
+        });                                                         
+                                         
+     }                                       
+}
