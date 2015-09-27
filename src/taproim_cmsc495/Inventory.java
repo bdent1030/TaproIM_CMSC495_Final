a8cb1478cbb635ccda7d06eaf7c96374c571eeca
194939 <194939@pn1243282.lanl.gov>
2015-09-27 15:35:32 -0600

    Updated

--- src/taproim_cmsc495/Inventory.java
+++ src/taproim_cmsc495/Inventory.java
@@ -18,25 +18,162 @@ import javax.swing.*;
 public class Inventory extends JFrame {
    
     //private variable associated with inventory objects
-    private String itemName;
+    private String itemID;
     private String description;
-    private int quantity;
-    private double price;
+    private String stockLevel;
+    private String weight;
     
+           
+    Connection con;
+    
+          
+    //  Establishing location, UID, password and sql command string
+    String url = "jdbc:mysql://siteground324.com:3306/gunnargo_cmsc495";
+    String userid = "gunnargo_umuc15";
+    String password = "Ib7t5BRa74mTr0N9aS6";
+    String sql = "SELECT * FROM gunnargo_cmsc495.Inventory";
 
     
-    public Inventory()
+
+        
+    public void setItem(String item, String desc, String amount, String value) {
+        this.itemID = item;
+        this.description = desc;
+        this.stockLevel = amount;
+        this.weight = value;
+    }
+       
+    public HashMap retrieveItem(int itemID){
+
+        HashMap<String, String> results = new HashMap<String, String>();
+        String sqlSelect = "SELECT * FROM gunnargo_cmsc495.Item = " + itemID;
+        
+        try {
+                con = DriverManager.getConnection(url, userid, password);
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(sqlSelect);
+                String item = rs.getString("ItemID");
+                String desc = rs.getString("description");
+                String amount = rs.getString("stockLevel");
+                String value = rs.getString("weight");
+                results.put("ItemID", item);
+                results.put("Description", desc);
+                results.put("StockLevel", amount);
+                results.put("Weight", value);
+                con.close();
+            } catch (SQLException ex) {
+                System.out.println("Item was not retrieved");
+            }
+        
+        return results;
+    }
+    
+    
+        public boolean updateItem(ArrayList<String> info){
+        
+        String item = info.get(0);
+        String desc = info.get(1);
+        String amount = info.get(2);
+        String value = info.get(3);
+        
+       
+            String sqlCreate = "UPDATE  gunnargo_cmsc495.Item SET "
+                    + "Item = " + item
+                    + " Description = " + desc
+                    + " StockLevel = " + amount
+                    + " Weight = " + value;
+        
+            try {
+                con = DriverManager.getConnection(url, userid, password);
+                Statement stmt = con.createStatement();
+                boolean success = stmt.execute(sqlCreate);
+                con.close();
+                return success;
+            } catch (SQLException ex) {
+                System.out.println("Item was not updated");
+                return false; // creation failed
+            }
+        }
+        
+       
+        
+       
+    public boolean newItem(ArrayList<String> info){
+        
+        String item = info.get(0);
+        String desc = info.get(1);
+        String amount = info.get(2);
+        String value = info.get(3);
+        
+        boolean result = false;
+        
+
+            String sqlCreate = "INSERT INTO gunnargo_cmsc495.Item SET "
+                    + "Item = " + item
+                    + " Description = " + desc
+                    + " StockLevel = " + amount
+                    + " Weight = " + value;
+        
+            try {
+                con = DriverManager.getConnection(url, userid, password);
+                Statement stmt = con.createStatement();
+                boolean success = stmt.execute(sqlCreate);
+                con.close();
+                return true;
+            } catch (SQLException ex) {
+                System.out.println("Item was not updated");
+                return false; // creation failed
+            }
+
+    }
+    
+       
+    public boolean deleteItem(String itemID){
+
+
+            String sqlSelect = "DELETE item FROM gunnargo_cmsc495.Item = " + itemID;
+            //boolean success;
+
+            try {
+                con = DriverManager.getConnection(url, userid, password);
+                Statement stmt = con.createStatement();
+                //success = stmt.execute(sqlSelect);
+                con.close();
+                return true;
+            } catch (SQLException ex) {
+                System.out.println("ItemID: " + itemID +"not found");
+                return false; // find failed
+            }
+        
+        
+    }
+        
+    public boolean itemExists(String itemID){
+        boolean result;
+        
+        String sqlSelect = "SELECT id FROM gunnargo_cmsc495.Item = " + itemID;
+
+        try {
+            con = DriverManager.getConnection(url, userid, password);
+            Statement stmt = con.createStatement();
+            result = stmt.execute(sqlSelect);
+            con.close();
+        } catch (SQLException ex) {
+            System.out.println("Customer ID: " + itemID +"not found");
+            return false; // find failed
+        }
+        
+        return result;
+    }
+    
+    public void showInventory()
     {
         this.setTitle("TAPRO-IM Inventory Table");
         ArrayList columnNames = new ArrayList();
         ArrayList data = new ArrayList();
+        String sql = "SELECT * FROM gunnargo_cmsc495.Item;";
 
-        //  Establishing location, UID, password and sql command string
-        String url = "jdbc:mysql://siteground324.com:3306/gunnargo_cmsc495";
-        String userid = "gunnargo_umuc15";
-        String password = "Ib7t5BRa74mTr0N9aS6";
-        String sql = "SELECT * FROM gunnargo_cmsc495.Inventory";
-        Connection con;
+
 
         // Try command to establish JDBC connection with above provided credentials
         try (Connection connection = DriverManager.getConnection( url, userid, password );
@@ -109,79 +246,7 @@ public class Inventory extends JFrame {
 
         JPanel buttonPanel = new JPanel();
         getContentPane().add( buttonPanel, BorderLayout.SOUTH );
-    }
-    
-    //default constructor  
-    public void setItem() {
-        itemName = "No name given";
-        description = "No description given";
-        quantity = 0;
-        price = 0.00;
-    }
-       
-    //non default constructor
-    public void setItem(String name, String desc, int amount, float value) {
-        itemName = name;
-        description = desc;
-        quantity = amount;
-        price = value;
-    }
-           
-    
-    //setter for itemName
-    public void setItemName(String newName) {
-        itemName = newName;
-    }
-        
-    //setter for category
-    public void setDescription(String newDescription) {
-        description = newDescription;
-    }
-    
-    //setter for vendorInventory
-    public void setQuantity(int newQuantity) {
-        if(newQuantity>0){
-            quantity = newQuantity;
-        }
-        else{
-            quantity = 0;
-        }
-    }
-
-    //setter for vendorPrice
-    public void setPrice(float newPrice) {
-        if(newPrice >0.00){
-            price = newPrice;
-        }
-        else{
-            price = 0.00;
-        }
-    }
-    
-    //getter for itemName
-    public String getItemName() {
-        return itemName;
-    }
-    
-    //getter for category
-    public String getDescription() {
-        return description;
-    }
-    
-    //getter for vendorInventory
-    public int getQuantity() {
-        return quantity;
-    }
-   
-    //getter for vendorPrice
-    public double getPrice() {
-        return price;
-    }
-    
-    //getter for value of item
-    public double getItemValue(){
-        return(price *(double)quantity);
-    }
-    
+            
+    }  
     
 }
