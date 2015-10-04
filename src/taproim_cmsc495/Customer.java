a99975fedc535e3de457628d96a1c5e558ce4b09
Gunnar Gorder <gunnargorder@gmail.com>
2015-10-03 17:30:52 -0700

    Removed showCustomerTable method and "extends JFrame" from class dec.

--- src/taproim_cmsc495/Customer.java
+++ src/taproim_cmsc495/Customer.java
@@ -15,7 +15,7 @@ import javax.swing.*;
 //CMSC 495 6380
 //9-27-15
 //Customer.java
-public class Customer extends JFrame {
+public class Customer{
     //  Establishing location, UID, password and sql command string
     String url = "jdbc:mysql://siteground324.com:3306/gunnargo_cmsc495";
     String userid = "gunnargo_umuc15";
@@ -184,86 +184,5 @@ public class Customer extends JFrame {
         }
     }
   
-    public void showCustomerTable()
-    {
-        this.setTitle("TAPRO-IM Customer Table");
-        ArrayList columnNames = new ArrayList();
-        ArrayList data = new ArrayList();
-
-        
-        String sql = "SELECT * FROM gunnargo_cmsc495.Customer;";
-
-        // Try command to establish JDBC connection with above provided credentials
-        try (Connection connection = DriverManager.getConnection( url, userid, password );
-            Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery( sql ))
-        {
-            ResultSetMetaData md = rs.getMetaData();
-            int columns = md.getColumnCount();
-
-            //  Get column names
-            for (int i = 1; i <= columns; i++)
-            {
-                columnNames.add( md.getColumnName(i) );
-            }
-
-            //  Get row data
-            while (rs.next()){
-                ArrayList row = new ArrayList(columns);
-
-                for (int i = 1; i <= columns; i++)
-                {
-                    row.add( rs.getObject(i) );
-                }
-
-                data.add( row );
-            }
-        }
-        catch (SQLException e){
-            System.out.println( e.getMessage() );}
-
-        // Create Vectors and copy over elements from ArrayLists to them
-        Vector columnNamesVector = new Vector();
-        Vector dataVector = new Vector();
-
-        for (int i = 0; i < data.size(); i++)
-        {
-            ArrayList subArray = (ArrayList)data.get(i);
-            Vector subVector = new Vector();
-            for (int j = 0; j < subArray.size(); j++)
-            {
-                subVector.add(subArray.get(j));
-            }
-            dataVector.add(subVector);
-        }
-
-        for (int i = 0; i < columnNames.size(); i++ )
-            columnNamesVector.add(columnNames.get(i));
-
-        //  Create table with database data    
-        JTable table = new JTable(dataVector, columnNamesVector)
-        {
-            public Class getColumnClass(int column)
-            {
-                for (int row = 0; row < getRowCount(); row++)
-                {
-                    Object o = getValueAt(row, column);
-
-                    if (o != null)
-                    {
-                        return o.getClass();
-                    }
-                }
-
-                return Object.class;
-            }
-        };
-
-        JScrollPane scrollPane = new JScrollPane( table );
-        getContentPane().add( scrollPane );
-
-        JPanel buttonPanel = new JPanel();
-        getContentPane().add( buttonPanel, BorderLayout.SOUTH );
-    }
-    
+   
 }
